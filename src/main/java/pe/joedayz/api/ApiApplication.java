2e6ef094fe81502b872fcba9d1db394e2b5905b7
Jose Diaz <jose.diaz@joedayz.pe>
2016-11-26 09:46:26 -0500

    usando config server

--- src/main/java/pe/joedayz/api/ApiApplication.java
+++ src/main/java/pe/joedayz/api/ApiApplication.java
@@ -1,12 +1,13 @@
 package pe.joedayz.api;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 }
